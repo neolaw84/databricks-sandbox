189775991e98d06cd72850bd1f6300d2424c33bc
neolaw84 <neolaw84>
2023-09-08 12:30:55 +0000

    end of day

---
+++ train/01_model_development.py
@@ -0,0 +1,148 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC # 01 Model Development Using *Pure `python`*
+# MAGIC
+# MAGIC In this notebook, we will see how a typical model development works using pure `python`. We will not use any databricks specific facility. 
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Problem
+# MAGIC We will use `california_housing` dataset to train models that can predict whether a given house is in top 25% most expensive houses.
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Preparations
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC
+# MAGIC **Import necessary libraries**
+# MAGIC
+# MAGIC First, we will import necessary libraries.
+
+# COMMAND ----------
+
+import pandas as pd
+
+from sklearn import datasets
+from sklearn import metrics
+from sklearn import model_selection
+from sklearn import linear_model, ensemble
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC **Fetch data**
+# MAGIC
+# MAGIC Next, we will fetch necessary data. 
+
+# COMMAND ----------
+
+df_X, ds_y = datasets.fetch_california_housing(return_X_y=True, as_frame=True)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC
+# MAGIC > Notice `databricks` can visualise and profile data in notebook without writing code.
+
+# COMMAND ----------
+
+display(df_X)
+
+# COMMAND ----------
+
+# MAGIC %md 
+# MAGIC
+# MAGIC **Train/Test split and labelling**
+# MAGIC
+# MAGIC Now, we will do train/test split and label the data.
+
+# COMMAND ----------
+
+X_tr, X_ts, y_tr, y_ts = model_selection.train_test_split(df_X, ds_y, test_size=0.2, random_state=42)
+percentile_75 = y_tr.describe()["75%"]
+
+y_tr_label = y_tr >= percentile_75
+y_ts_label = y_ts >= percentile_75
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC
+# MAGIC **Models and parameters**
+# MAGIC
+# MAGIC Now, we will prepare some models and parameters. 
+# MAGIC
+# MAGIC For simplicity, we will use `SGDClassifier` and `GradientBoostingClassifier` with 2 sets of parameters each. This will give us a 4 combinations of models and parameters. 
+
+# COMMAND ----------
+
+param_log_reg = {"loss": "log_loss", "penalty": "elasticnet"}
+param_svm = {"loss": "hinge", "penalty": "elasticnet"}
+
+model_log_reg = linear_model.SGDClassifier(**param_log_reg)
+model_svm = linear_model.SGDClassifier(**param_svm)
+
+param_fast_large = {"n_estimators": 100, "learning_rate": 0.15}
+param_slow_small = {"n_estimators": 80, "learning_rate": 0.1}
+
+model_fast_large = ensemble.GradientBoostingClassifier(**param_fast_large)
+model_slow_small = ensemble.GradientBoostingClassifier(**param_slow_small)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC
+# MAGIC **Getting ready to train/test**
+# MAGIC
+# MAGIC Finally, we prepare a utility function to train and test.
+
+# COMMAND ----------
+
+def train_and_test(model, tr_X = X_tr, tr_label = y_tr_label, ts_X = X_ts, ts_label = y_ts_label):
+    print (model)
+    model.fit(tr_X, tr_label)
+    y_pred = model.predict(ts_X)
+    print (f"Precision : {metrics.precision_score(ts_label, y_pred)}")
+    print (f"Recall : {metrics.recall_score(ts_label, y_pred)}")
+    print (f"f1 score : {metrics.f1_score(ts_label, y_pred)}")
+    print ("Confusion Matrix")
+    print (metrics.confusion_matrix(ts_label, y_pred))
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC
+# MAGIC ## Training and Testing
+
+# COMMAND ----------
+
+train_and_test(model_log_reg)
+
+# COMMAND ----------
+
+train_and_test(model_svm)
+
+# COMMAND ----------
+
+train_and_test(model_fast_large)
+
+# COMMAND ----------
+
+train_and_test(model_slow_small)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC
+# MAGIC ## Conclusion
+# MAGIC
+# MAGIC We learnt that tracking the models, (training) parameters and (test) performance using **pure python** is cumbersome and error-prone. 
+
+# COMMAND ----------
+
+
---
+++ train/02_model_development_with_mlflow.py
@@ -0,0 +1,180 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC
+# MAGIC # 02 Model Development with *`mlflow`*
+# MAGIC
+# MAGIC In this notebook, we will see how a typical model development works using `mlflow`. `mlflow` is a facility provided by default in a databricks environment. 
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Problem
+# MAGIC We will use `california_housing` dataset to train models that can predict whether a given house is in top 25% most expensive houses.
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Preparations
+
+# COMMAND ----------
+
+# change 1 : we need to import mlflow
+import mlflow
+
+import yaml 
+
+import pandas as pd
+
+from sklearn import datasets
+from sklearn import metrics
+from sklearn import model_selection
+from sklearn import linear_model, ensemble
+
+# COMMAND ----------
+
+df_X, ds_y = datasets.fetch_california_housing(return_X_y=True, as_frame=True)
+
+X_tr, X_ts, y_tr, y_ts = model_selection.train_test_split(df_X, ds_y, test_size=0.2, random_state=42)
+percentile_75 = y_tr.describe()["75%"]
+
+y_tr_label = y_tr >= percentile_75
+y_ts_label = y_ts >= percentile_75
+
+# COMMAND ----------
+
+param_log_reg = {"loss": "log_loss", "penalty": "elasticnet"}
+param_svm = {"loss": "hinge", "penalty": "elasticnet"}
+
+model_log_reg = linear_model.SGDClassifier(**param_log_reg)
+model_svm = linear_model.SGDClassifier(**param_svm)
+
+param_fast_large = {"n_estimators": 100, "learning_rate": 0.15}
+param_slow_small = {"n_estimators": 80, "learning_rate": 0.1}
+
+model_fast_large = ensemble.GradientBoostingClassifier(**param_fast_large)
+model_slow_small = ensemble.GradientBoostingClassifier(**param_slow_small)
+
+# COMMAND ----------
+
+def train_and_test_with_mlflow(model, run_name, tr_X = X_tr, tr_label = y_tr_label, ts_X = X_ts, ts_label = y_ts_label):
+    # change 2 : use mlflow.start_run context manager
+    with mlflow.start_run(run_name=run_name):
+        model.fit(tr_X, tr_label)
+        y_pred = model.predict(ts_X)
+        # change 3 : log the metrics
+        mlflow.log_metric(key="precision", value=metrics.precision_score(ts_label, y_pred))
+        # print (f"Precision : {metrics.precision_score(ts_label, y_pred)}")
+        mlflow.log_metric(key="recall", value=metrics.recall_score(ts_label, y_pred))
+        # print (f"Recall : {metrics.recall_score(ts_label, y_pred)}")
+        mlflow.log_metric(key="f1", value=metrics.f1_score(ts_label, y_pred))
+        # print (f"f1 score : {metrics.f1_score(ts_label, y_pred)}")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC
+# MAGIC ## Training and Testing
+
+# COMMAND ----------
+
+train_and_test_with_mlflow(model_log_reg, "log_reg")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC
+# MAGIC **More than just metrics**
+# MAGIC
+# MAGIC Actually, `mlflow` can help us track parameters, datasets, and model artifacts in addition to just metrics.
+
+# COMMAND ----------
+
+conda_env = mlflow.sklearn.get_default_conda_env(include_cloudpickle=True)
+with open("../configurations/conda_env.yaml", "wt") as f:
+    yaml.dump(conda_env, f)
+
+# COMMAND ----------
+
+def train_and_test_with_mlflow(model, run_name, params, tr_X = X_tr, tr_label = y_tr_label, ts_X = X_ts, ts_label = y_ts_label):
+    # change 2 : use mlflow.start_run context manager
+    with mlflow.start_run(run_name=run_name):
+        # change 3.2 log the parameters
+        mlflow.log_params(params)
+
+        # change 3.3 log the training dataset
+        mlflow.log_input(mlflow.data.from_pandas(tr_X), context="training-X")
+        mlflow.log_input(mlflow.data.from_numpy(tr_label.to_numpy()), context="training-y")
+
+        model.fit(tr_X, tr_label)
+        y_pred = model.predict(ts_X)
+        # change 3.1 log the metrics
+        mlflow.log_metrics({
+            "precision" : metrics.precision_score(ts_label, y_pred),
+            "recall" : metrics.recall_score(ts_label, y_pred),
+            "f1" : metrics.f1_score(ts_label, y_pred)
+        })
+        
+        # change 3.4 log the model itself
+        mlflow.sklearn.log_model(
+            model, run_name,
+            conda_env="../configurations/conda_env.yaml",
+            signature=mlflow.models.infer_signature(tr_X, y_pred)
+        )
+
+# COMMAND ----------
+
+train_and_test_with_mlflow(model_log_reg, run_name="log_reg_2", params=param_log_reg)
+
+# COMMAND ----------
+
+train_and_test_with_mlflow(model_svm, run_name="svm", params=param_svm)
+
+# COMMAND ----------
+
+train_and_test_with_mlflow(model_fast_large, run_name="gbm_fast_large", params=param_fast_large)
+
+# COMMAND ----------
+
+train_and_test_with_mlflow(model_slow_small, run_name="gbm_slow_small", params=param_slow_small)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC
+# MAGIC **Even how metrics progresses**
+# MAGIC
+# MAGIC In the example below, we will illustrate how `mlflow` can be used to track progression of metrics.
+
+# COMMAND ----------
+
+from sklearn import neural_network
+
+net = neural_network.MLPClassifier(
+    hidden_layer_sizes=(20, 20), 
+    max_iter=25,
+    warm_start=True
+)
+
+warm_start=False
+
+with mlflow.start_run(run_name="neural_network"):
+    for i in range(0, 10):
+        net.fit(X_tr, y_tr_label)
+        y_pred = net.predict(X_ts)
+        mlflow.log_metrics({
+            "precision" : metrics.precision_score(y_ts_label, y_pred),
+            "recall" : metrics.recall_score(y_ts_label, y_pred),
+            "f1" : metrics.f1_score(y_ts_label, y_pred)
+        }, step=net.n_iter_)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC
+# MAGIC ## Conclusion
+# MAGIC
+# MAGIC We learnt that tracking the models, (training) parameters and (test) performance as well as datasets using **`mlflow`** is easy and straightforward. It reduces errors and is less cumbersome. Finally, it facilitates recording of experiments with minimum changes to workflows. 
+
+# COMMAND ----------
+
+
